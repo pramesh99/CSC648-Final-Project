28ac028e4c49565cfd64d98109f41bd469801eac
pramesh99 <70615318+pramesh99@users.noreply.github.com>
2023-02-23 17:41:50 -0800

    adding backend files, modifying frontend package.json and app.js for testing

--- application/frontEnd/my-app/src/App.js
+++ application/frontEnd/my-app/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [serverData, setServerData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/test").then(
+      response => response.json()
+    ).then(
+      data => {
+        setServerData(data)
+      }
+    )
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <div style={{textAlign:"center"}}>
+          {(typeof serverData.users === "undefined") ? (
+            <p>Loading...</p>
+          ) : (
+            serverData.users.map((user, i) => (
+              <p key={i}>{user}</p>
+            ))
+          )}
+        </div>
       </header>
     </div>
   );
