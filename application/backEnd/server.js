06f873a767412075e5d59bfc25c5084725de6228
Preetham Ramesh <70615318+pramesh99@users.noreply.github.com>
2023-02-24 20:57:24 -0800

    updated paths

--- application/backEnd/server.js
+++ application/backEnd/server.js
@@ -4,13 +4,15 @@ const app = express();
 
 const users = ['Preetham', 'Shauhin', 'Hieu', 'Derrick', 'Lin'];
 
+app.use(express.static(path.resolve(__dirname, '../frontEnd/my-app/build')));
+
 app.get('/test', (req, res) => {
     console.log('at test');
     res.json({'users': users});
 })
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontEnd/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontEnd/my-app/build', 'index.html'));
 })
 
 app.listen(3001, () => {console.log("Server started on port 3001")});
