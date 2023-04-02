2bdc8a4c95ac21a07be49f1db646c1fced357ea7
Hieu Ma <97485726+Hieu-Ma1@users.noreply.github.com>
2023-04-02 16:15:58 -0700

    restyled navbar

--- application/frontEnd/my-app/src/components/navbar/Navbar.js
+++ application/frontEnd/my-app/src/components/navbar/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
-function Navbar (props) {
+function Navbar(props) {
    return (
-      <div id="navbar">
-         <div id="home-icon">
-            <Link to="/" style={{ textDecoration: 'none' }}>Team 3</Link>
+      <div id={styles["navbar"]}>
+         <div id={styles["home-icon"]}>
+            <Link to="/" id={styles["icon-text"]}>Team 3 : Gator Grub</Link>
          </div>
-         <div id="right-buttons">
-            <Link to="/aboutUs" style={{ textDecoration: 'none' }}>About Us</Link>
+         <div id={styles["search-bar"]}>
+            <select id={styles["search-dropdown"]}>
+               <option value="italian">Italian</option>
+               <option value="chinese">Chinese</option>
+               <option value="american">American</option>
+            </select>
+            <div id={styles["search-separator"]}></div>
+            <input type="text" id={styles["search-input"]} name="search" maxlength="80" size="80" required />
+            <div id={styles["search-icon-container"]}>
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101" id={styles["search-icon"]}><path d="M63.3 59.9c3.8-4.6 6.2-10.5 6.2-17 0-14.6-11.9-26.5-26.5-26.5S16.5 28.3 16.5 42.9 28.4 69.4 43 69.4c6.4 0 12.4-2.3 17-6.2l20.6 20.6c.5.5 1.1.7 1.7.7.6 0 1.2-.2 1.7-.7.9-.9.9-2.5 0-3.4L63.3 59.9zm-20.4 4.7c-12 0-21.7-9.7-21.7-21.7s9.7-21.7 21.7-21.7 21.7 9.7 21.7 21.7-9.7 21.7-21.7 21.7z"></path></svg>
+            </div>
+         </div>
+         <div id={styles["right-buttons"]}>
+            <Link to="/aboutUs" id={styles["right-buttons-text"]}>About Us</Link>
          </div>
       </div>
    )
