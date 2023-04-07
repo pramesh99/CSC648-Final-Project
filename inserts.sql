8117704b73f5f2a9c4736d450133bd6dc7f81f50
Shauhin Pourshayegan <64994784+LimeSauc3@users.noreply.github.com>
2023-04-06 22:35:18 -0700

    created insert statements, populating entities needed for M2 deliverable

--- inserts.sql
+++ inserts.sql
@@ -4,24 +4,38 @@
 
 USE gatorGrubDB;
 
-INSERT INTO RestaurantOwner (RestaurantOwnerID, RestaurantOwnerName, RestaurantOwnerEmail, RestaurantOwnerPhone, RestaurantOwnerPassword)
-VALUES (1, "Bob Smith", "bobsmith@sfsu.com", "1231231234", "password8"),
-		(2, "Billy Smith", "bobsmith@sfsu.com", "1231231234", "password8"),
-        (3, "Billy Johnson", "bobsmith@sfsu.com", "1231231234", "password8"),
-        (4, "Greg Elliot", "bobsmith@sfsu.com", "1231231234", "password8"),
-        (5, "Sarah Sanders", "bobsmith@sfsu.com", "1231231234", "password8"),
-        (6, "Joe Rogen", "bobsmith@sfsu.com", "1231231234", "password8"),
-        (7, "Johnjacobjingleheimer Schmidt", "bobsmith@sfsu.com", "1231231234", "password8")
+INSERT INTO RestaurantOwner (RestaurantOwnerName, RestaurantOwnerEmail, RestaurantOwnerPhone, RestaurantOwnerPassword)
+VALUES ("Bob Smith", "bobsmith@sfsu.com", "1231231234", "password8!"),
+	("Billy Smith", "billysmith@sfsu.com", "1231231234", "password8!"),
+	("Billy Johnson", "billyjohnson@sfsu.com", "1231231234", "password8!"),
+	("Greg Elliot", "gregelliot@sfsu.com", "1231231234", "password8!"),
+	("Sarah Sanders", "sarahsanders@sfsu.com", "1231231234", "password8!"),
+	("Joe Rogen", "joerogen@sfsu.com", "1231231234", "password8!"),
+	("Johnjacobjingleheimer Schmidt", "johnjacobjingleheimerschmidt@sfsu.com", "1231231234", "password8!")
 ;
 
-INSERT INTO Restaurant (RestaurantID, RestaurantOwner, RestaurantRegistered, RestaurantName, RestaurantPhone, RestaurantPassword, 
-						RestaurantAddress, RestaurantCuisine, RestaurantPriceTier, RestaurantHours, RestaurantPrepTime) 
-VALUES (1, "Bob Smith", "1", "Eating Place", "1234567890", "password1", "2735 39th Ave, San Francisco, CA 94116", "American", "3", "", "25"),
-		(2, "Billy Smith", "1", "Eating Location", "1234567890", "password2", "2735 39th Ave, San Francisco, CA 94116", "American", "5", "", "25"),
-        (3, "Billy Johnson", "1", "Feasting Area", "1234567890", "password3", "2735 39th Ave, San Francisco, CA 94116", "Italian", "1", "", "25"),
-        (4, "Greg Elliot", "1", "Drinking locale", "1234567890", "password4", "2735 39th Ave, San Francisco, CA 94116", "Italian", "2", "", "25"),
-        (5, "Sarah Sanders", "1", "Sipping Street", "1234567890", "password5", "2735 39th Ave, San Francisco, CA 94116", "Indian", "5", "", "25"),
-        (6, "Joe Rogen", "1", "Gobbling Garden", "1234567890", "password6", "2735 39th Ave, San Francisco, CA 94116", "Indian", "4", "", "25"),
-        (7, "Johnjacobjingleheimer Schmidt", "1", "Stuffing Square", "1234567890", "password7", "2735 39th Ave, San Francisco, CA 94116", "Indian", "2", "", "25")
+INSERT INTO Driver (DriverName, DriverEmail, DriverPhone, DriverPassword)
+VALUES ("Nathan Luu", "nathanluu@gmail.com", "1231231234", "password8!"),
+	("Daniel Schutz", "danielschutz@gmail.com", "1231231234", "password8!"),
+    ("Jerry Williams", "jerrywilliams@gmail.com", "1231231234", "password8!"),
+    ("Rolf Kleinfeldt", "rolfkleinfeldt@gmail.com", "1231231234", "password8!"),
+    ("Gabriella Tong", "gabbytong@gmail.com", "1231231234", "password8!"),
+    ("Ana Warner", "anawarner@gmail.com", "1231231234", "password8!"),
+    ("Hadas Curry", "hadascurry@gmail.com", "1231231234", "password8!"),
+    ("Bea Joosten", "beajoosten@gmail.com", "1231231234", "password8!"),
+    ("Anton Haight", "antonhaight@gmail.com", "1231231234", "password8!"),
+    ("Tonio Stenger", "toniostenger@gmail.com", "1231231234", "password8!"),
+    ("Catrina Oliver", "catrinaoliver@gmail.com", "1231231234", "password8!")
+;
+
+INSERT INTO Restaurant (RestaurantOwnerID, RestaurantRegistered, RestaurantName, RestaurantPhone, RestaurantPassword, 
+						RestaurantAddress, RestaurantCuisine, RestaurantPriceTier, RestaurantHours, RestaurantPrepTime RestaurantImage RestaurantCoordinates) 
+VALUES (1, 1, "Eating Place", "1234567890", "password1", "155 Winston Dr, San Francisco, CA 94132", "American", "3", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+		(2, 1, "Eating Location", "1234567890", "password2", "155 Winston Dr, San Francisco, CA 94132", "American", "5", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+        (3, 1, "Feasting Area", "1234567890", "password3", "155 Winston Dr, San Francisco, CA 94132", "Italian", "1", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+        (4, 1, "Drinking locale", "1234567890", "password4", "155 Winston Dr, San Francisco, CA 94132", "Italian", "2", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+        (5, 1, "Sipping Street", "1234567890", "password5", "155 Winston Dr, San Francisco, CA 94132", "Indian", "5", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+        (6, 1, "Gobbling Garden", "1234567890", "password6", "155 Winston Dr, San Francisco, CA 94132", "Indian", "4", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744'),
+        (7, 1, "Stuffing Square", "1234567890", "password7", "155 Winston Dr, San Francisco, CA 94132", "Indian", "2", "", "25", "./src/images/restaurant/12345", '37.7266,-122.4744')
 ;
 
